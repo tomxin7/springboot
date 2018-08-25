26c2b52879dcafac67526c33a1bf3a7c53aed3ab
tomxin <865498311@163.com>
2018-08-25 12:51:44 +0800

    新增了对分页的支持

--- src/main/java/cn/tomxin/springboot/controller/DemoController.java
+++ src/main/java/cn/tomxin/springboot/controller/DemoController.java
@@ -1,24 +1,43 @@
 package cn.tomxin.springboot.controller;
 
 import cn.tomxin.springboot.entity.Demo;
+import cn.tomxin.springboot.mapper.DemoMapper;
 import cn.tomxin.springboot.service.DemoService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class DemoController {
 
     @Autowired
     DemoService userService;
-    @RequestMapping("/")
     /**
      * 测试数据库连通后，请务必取消该查询数据库方法，改为返回一个 hello world
      */
+    @RequestMapping("/")
     public List<Demo> index() throws Exception {
-        List<Demo> userList = userService.findAll();
-        return userList;
+        List<Demo> demoList = userService.findAll();
+        return demoList;
+    }
+
+    /**
+     * 分页查询，pageNum传0和传1是一样的
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping(value = "/demo/{pageNum}/{pageSize}", method = RequestMethod.GET)
+    public List<Demo> findByPage(@PathVariable Integer pageNum, @PathVariable Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<Demo> demoList = userService.findAll();
+        return demoList;
     }
 }
